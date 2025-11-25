c53b698b2451e820862d378731abd3985cab1911
Nichollas777 <nichollas932@gmail.com>
2025-11-25 20:36:12 -0300

    configuração do typeorm e .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST')!,
+        port: parseInt(config.get<string>('DB_PORT')!),
+        username: config.get<string>('DB_USERNAME')!,
+        password: config.get<string>('DB_PASSWORD')!,
+        database: config.get<string>('DB_NAME')!,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),    
+  ],
 })
 export class AppModule {}
